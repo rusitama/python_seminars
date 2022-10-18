25004cc5d752ef73706b7acd5d586c9f374f3e27
rustam <rusitama@gmail.com>
2022-10-18 17:02:25 +0500

    Add homework4 session 2

---
+++ Session2_homework4.py
@@ -0,0 +1,17 @@
+"""Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+Найдите произведение элементов на позициях a и b.
+Значения N, a и b вводит пользователь с клавиатуры."""
+
+number = int(input("Введите число: "))
+a = int(input("Введите для нахождения позиции a c 0 до " + str(number * 2) + ": "))
+b = int(input("Введите для нахождения позиции b c 0 до " + str(number * 2) + ": "))
+l = []
+n = number + 1
+for i in range(number):
+    l.append(-1 * (n - 1))
+    n -= 1
+l.append(0)
+for i in range(number):
+    l.append(i + 1)
+print(l)
+print(l[a] * l[b])
